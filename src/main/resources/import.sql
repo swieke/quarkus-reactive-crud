c9678d5594a3f9802ddb88e5635169dd2adf7e02
Theo Azriel <theoazrie@gmail.com>
2023-08-08 23:51:30 +0800

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into product(title,description,createdAt,updatedAt) values ('Product1','description',NOW(),NOW());
+insert into product(title,description,createdAt,updatedAt) values ('Product2','description',NOW(),NOW());
+insert into product(title,description,createdAt,updatedAt) values ('Product3','description',NOW(),NOW());
+insert into product(title,description,createdAt,updatedAt) values ('Product4','description',NOW(),NOW());
